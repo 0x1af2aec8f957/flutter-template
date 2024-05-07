3f6e2f86876210256996d63624faff0ed34707db
Abner <0x23fd5097@gmail.com>
2024-05-07 17:57:08 +0800

    feat: 新增空间清理

--- lib/views/Storage.dart
+++ lib/views/Storage.dart
@@ -3,10 +3,23 @@ import 'package:path_provider/path_provider.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 import '../utils/common.dart';
+import '../plugins/dialog.dart';
 
 class Storage extends StatelessWidget {
   final String title;
   Storage({Key? super.key, required this.title});
+
+  Future<bool> handleClearApplicationDocumentsDirectory() => Talk.alert('文档数据不可被清除', title: '警告', isCancel: false); // 清空文档
+
+  Future<void> handleClearTemporaryDirectory() => Talk.sheetAlert('确定清除缓存数据吗？清除缓存数据会清楚被缓存的聊天文件及临时数据').then((bool isOk) => isOk ? DefaultCacheManager().emptyCache() : null); // 清空缓存
+
+  Future<void> handleClearDownloadsDirectory() async { // 清空下载
+    final bool isOk = await Talk.sheetAlert('确定清除下载数据吗？清除下载数据会清楚下载的聊天文件');
+    if (!isOk) return;
+
+    final dir = await getDownloadsDirectory();
+    dir?.delete(recursive: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +42,10 @@ class Storage extends StatelessWidget {
                 subtitle: Text('应用程序使用的主要目录，用于存储用户生成的数据', style: TextStyle(fontSize: 12)),
                 trailing: FutureBuilder<int>(
                   future: getApplicationDocumentsDirectory().then((result) => result.fileSize),
-                  builder: (context, snapshot) => snapshot.connectionState != ConnectionState.done ? CircularProgressIndicator() : Text(snapshot.data?.size ?? StringHelper.placeholder),
+                  builder: (context, snapshot) => snapshot.connectionState != ConnectionState.done ? CircularProgressIndicator() : GestureDetector(
+                    child: Text(snapshot.data?.size ?? StringHelper.placeholder),
+                    onDoubleTap: handleClearApplicationDocumentsDirectory,
+                  ),
                 )
               ),
             ),
@@ -43,7 +59,7 @@ class Storage extends StatelessWidget {
                   future: getTemporaryDirectory().then((result) => result.fileSize),
                   builder: (context, snapshot) => snapshot.connectionState != ConnectionState.done ? CircularProgressIndicator() : GestureDetector(
                     child: Text(snapshot.data?.size ?? StringHelper.placeholder),
-                    onDoubleTap: () => DefaultCacheManager().emptyCache(), // 清空缓存
+                    onDoubleTap: handleClearTemporaryDirectory, // 清空缓存
                   ),
                 ),
               ),
@@ -56,7 +72,10 @@ class Storage extends StatelessWidget {
                 subtitle: Text('应用程序可以访问的下载目录，用于存储下载产生的数据', style: TextStyle(fontSize: 12)),
                 trailing: FutureBuilder<int?>(
                   future: getDownloadsDirectory().then((result) => result?.fileSize),
-                  builder: (context, snapshot) => snapshot.connectionState != ConnectionState.done ? CircularProgressIndicator() : Text(snapshot.data?.size ?? StringHelper.placeholder),
+                  builder: (context, snapshot) => snapshot.connectionState != ConnectionState.done ? CircularProgressIndicator() : GestureDetector(
+                    child: Text(snapshot.data?.size ?? StringHelper.placeholder),
+                    onDoubleTap: handleClearDownloadsDirectory,
+                  ),
                 )
               ),
             ),
